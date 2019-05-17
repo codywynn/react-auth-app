c3a1a660067fa4031ef736b5ab08d3cd77a4b12f
Cody <codywynn00@gmail.com>
2019-05-16 22:00:25 -0700

    Add login and register routes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 const dotenv = require('dotenv');
 dotenv.config();
+
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -26,6 +29,15 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
-const port = process.env.PORT; // process.env.port is Heroku's port if you choose to deploy the app there
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
