dc74b477a301c7499a6318688ca283de3824b5e9
Cody <codywynn00@gmail.com>
2019-05-18 23:28:19 -0700

    Add layout and authentication components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <h1>
-      Hello World
-    </h1>
-  );
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
